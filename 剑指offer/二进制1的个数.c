20cff54bd593d4c1e4de4d9e10259d6d7f24611b
passerby33 <49405656+passerby33@users.noreply.github.com>
2019-05-20 23:56:00 +0800

    剑指offer

---
+++ 剑指offer/二进制1的个数.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <stdlib.h>
+//int fun_bit(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n&(n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	printf("%d", fun_bit(n));
+//	system("pause");
+//	return 0;
+//}
