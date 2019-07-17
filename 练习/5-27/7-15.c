ea77874d307b9db3ebdba9cea499d891a753401b
passerby33 <49405656+passerby33@users.noreply.github.com>
2019-07-17 13:53:54 +0800

    练习

---
+++ 练习/5-27/7-15.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+//typedef struct node{
+//	char val;
+//	struct node* left;
+//	struct node* right;
+//}node;
+//void qianxu(node*root)
+//{
+//	if (root == NULL){
+//		return;
+//	}
+//	printf("%c", root->val);//¸ù
+//	qianxu(root->left);
+//	qianxu(root->right);
+//}
+//void zhongxu(node*root){
+//	if (root == NULL){
+//		return;
+//	}
+//	zhongxu(root->left);
+//	printf("%c", root->val);
+//	zhongxu(root->right);
+//}
+//void houxu(node*root){
+//	if (root == NULL){
+//		return;
+//	}
+//	houxu(root->left);
+//	houxu(root->right);
+//	printf("%c", root->val);
+//}
+//	
+//int main()
+//{
+//	system("pause");
+//	return 0;
+//}
