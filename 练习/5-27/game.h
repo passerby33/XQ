e0464aafccc132793c610811848564ccd6136e64
passerby33 <49405656+passerby33@users.noreply.github.com>
2019-09-07 11:56:22 +0800

    练习

---
+++ 练习/5-27/game.h
@@ -0,0 +1,12 @@
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define ROW 9
+#define COL 9
+#define ROWS 11
+#define COLS 11
+void initboard(char board[ROWS][COLS], int row, int col,char ret);
+void disboard(char board[ROWS][COLS], int row, int col);
+
