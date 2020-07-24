8ce1c137be5a23405ccf165b24a3bb8988b5c2ac
passerby33 <49405656+passerby33@users.noreply.github.com>
2020-07-24 18:25:49 +0800

    练习

---
+++ 练习/6-16/作业.c
@@ -0,0 +1,52 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#include <stdlib.h>
+//int main()
+//{
+//	/*char a[] = "";
+//	int count = 0,flag=0;
+//	gets(a);
+//	for (int i = 0; i<strlen(a); i++)
+//	{
+//		if (a[i] == '"'&&flag==0)
+//		{
+//			
+//			flag++;
+//		}
+//		if (a[i] == '"'&&flag==1)
+//		{
+//			
+//			flag--;
+//			count++;
+//		}
+//		if (a[i] == ' '&&flag == 0)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d\n", count + 1);
+//	for (int i = 0; i<strlen(a); i++)
+//	{
+//		if (a[i] == '"')
+//		{
+//			int j = 0;
+//			for ( j = i + 1; j != '"'; j++)
+//			{
+//				printf("%c", a[j]);
+//			}
+//			i = j+1;
+//			continue;
+//		}
+//		if (a[i] == ' ')
+//		{
+//			printf("\n");
+//			continue;
+//		}
+//		printf("%c", a[i]);
+//	}
+//	printf("\n");*/
+//
+//
+//	system("pause");
+//	return 0;
+//}
