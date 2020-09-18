9e3741f5f6dd1a3352776d6a4bcf0e55ad6cbc9c
passerby33 <49405656+passerby33@users.noreply.github.com>
2020-09-18 19:17:19 +0800

    练习

---
+++ 练习/6-16/22.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cmath>
+#include <cWindows>
+using namespace std;
+#if 0
+int main()
+{
+
+	long int n = 0;
+	int count;
+	while (cin >> n&&n != 0)
+	{
+		count = 0;
+		while (n != 1 && n != 0)
+		{
+			if (n % 3 == 0)
+			{
+				n /= 3;
+				count++;
+			}
+			else if (n % 3 == 1)
+			{
+				n += 2;
+				n /= 3;
+				count++;
+			}
+			else if (n % 3 == 2)
+			{
+				n += 1;
+				n /= 3;
+				count++;
+			}
+		}
+		cout << count << endl;
+	}
+
+	int n = 0;
+	int sum = 0;
+	while (cin >> n&&n != 0)
+	{
+		sum = 0;
+		for (int i = 2; i < sqrt(n); i++)
+		{
+			if (n%i == 0)
+			{
+				sum++;
+				while (n%i != 0)
+				{
+					n /= i;
+				}
+			}
+		}
+		if (sqrt(n) < n)
+		{
+			sum++;
+		}
+		cout << sum << endl;
+	}
+	return 0;
+}
+#endif
