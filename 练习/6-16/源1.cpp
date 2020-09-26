7b10ff8274d0ddf75641f54b7390170293c8bf9b
passerby33 <49405656+passerby33@users.noreply.github.com>
2020-09-26 12:15:01 +0800

    练习

---
+++ 练习/6-16/源1.cpp
@@ -0,0 +1,81 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 0;
+//	int j = 2;
+//	int count = 0;
+//	int temp = 1;
+//	while (cin >> n)
+//	{
+//		for (int i = 2; i <= n; i++)
+//		{
+//			temp = 1;
+//			for (j = 2; j < sqrt(i); j++)
+//			{
+//				if (i%j == 0)
+//				{
+//					temp += j;
+//					temp += i / j;
+//				}
+//			}
+//			if (temp == i)
+//			{
+//				count++;
+//			}
+//		}
+//		cout << count<<endl;
+//	}
+//	return 0;
+//}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 0;
+//	int j = 0;
+//	int count = 0;
+//	int i = 28;
+//	int temp = 1;
+//	for (j = 2; j < sqrt(28); j++)
+//	{
+//		if (i%j == 0)
+//		{
+//			temp += j;
+//			temp += i / j;
+//		}
+//	}
+//	cout << temp<<endl;
+//	return 0;
+//}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 0;
+//	int j = 2;
+//	int count = 0;
+//	int temp = 1;
+//	while (cin >> n)
+//	{
+//		for (int i = 2; i <= n; i++)
+//		{
+//			count = 0;
+//			//temp = 1;
+//			for (; j < sqrt(i); j++)
+//			{
+//				if (i%j == 0)
+//				{
+//					temp += j;
+//					temp += i / j;
+//				}
+//			}
+//			if (temp == i)
+//			{
+//				count++;
+//			}
+//		}
+//		cout << count<<endl;
+//	}
+//	return 0;
+//}
