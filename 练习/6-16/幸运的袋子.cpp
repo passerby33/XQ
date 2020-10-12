713c6bcb96588df18b277fb848b820d92eb39ce0
passerby33 <49405656+passerby33@users.noreply.github.com>
2020-10-12 12:34:21 +0800

    练习

---
+++ 练习/6-16/幸运的袋子.cpp
@@ -0,0 +1,23 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n = 0;
+//	cin >> n;
+//	int *p = (int*)malloc(sizeof(int)*n);
+//	int x1 = 0, x2 = 1, count = 0;
+//	for (int i = 0; i<n; i++)
+//	{
+//		cin >> p[i];
+//		x1 += p[i];
+//		x2 *= p[i];
+//			if (x1>x2)
+//			{
+//				count++;
+//			}
+//	}
+//	cout << count << endl;
+//	free(p);
+//	return 0;
+//
+//}
