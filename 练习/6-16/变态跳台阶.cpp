317c23c8ec9dd546b14bc6edcfc32e0eefca9ba5
passerby33 <49405656+passerby33@users.noreply.github.com>
2020-10-13 19:39:12 +0800

    练习

---
+++ 练习/6-16/变态跳台阶.cpp
@@ -0,0 +1,38 @@
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+	//int number = 0;
+	//cin >> number;
+	//	int x = 1, y = 1;
+	//	int num = 1;
+	//	if (number == 1)
+	//	{
+	//		cout << 1 << endl;
+	//		system("pause");
+	//		return 0;
+	//	}
+	//	int sum = 1;
+	//	for (int i = 2; i<number; i++)
+	//	{
+	//		num = x + y;
+	//		y = x;
+	//		x = num;
+	//		sum += x;
+	//	}
+	//	cout << sum + 1 << endl;	
+	//	system("pause");
+	//	return 0;
+//	double x = 0, y = 0;
+//	cin >> x >> y;
+//	if (x / y < 6.24)
+//	{
+//		cout << "Yes" << endl;
+//
+//	}
+//	else{
+//		cout << "No" << endl;
+//	}
+//	system("pause");
+//	return 0;
+//}
