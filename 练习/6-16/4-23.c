3632769340ac5b09a7dcfc230e8c3ee42cf187c7
passerby33 <49405656+passerby33@users.noreply.github.com>
2020-12-15 20:18:48 +0800

    练习

---
+++ 练习/6-16/4-23.c
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+//计算整形二进制中1出现的个数
+//int count_one_bits(unsigned int value)
+//{
+//	int count = 0;
+//	while (value)
+//	{
+//		if (value & 1 == 1)  
+//		{
+//			count++;
+//		}
+//		value=value >> 1;
+//	}
+//	return count;
+//	// 返回 1的位数 
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int ret = count_one_bits(a);
+//	printf("出现1的个数:%d\n", ret);
+//	system("pause");
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	for (i = 0; i < 16; i++)
+//	{
+//		
+//	}
+//}
